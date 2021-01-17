1f9b495ef62dc2f6785531f3a9e40dc4d1331ed3
jakdillard <jakdillard@gmail.com>
2021-01-17 04:03:32 -0500

    lets let aosp be great
    this reverts drop aosp prefix hack
    
    Change-Id: I13ac694a44938f739f4ecdea9b94f89f6d55a29b

--- aosp_bramble.mk
+++ aosp_bramble.mk
@@ -69,5 +69,6 @@ PRODUCT_MODEL := Pixel 4a 5G
 
 # Match stock product info
 PRODUCT_OVERRIDE_INFO := true
+PRODUCT_OVERRIDE_NAME := bramble
 PRODUCT_OVERRIDE_FINGERPRINT := google/bramble/bramble:11/RQ1A.210105.003/7002689:user/release-keys
 PRODUCT_OVERRIDE_DESC := bramble-user 11 RQ1A.210105.003 7002689 release-keys
